1d53296bbae559846145f5fe88e8d83b52a2a267
Ilmira Sitdikova <ilmira.sitdikova.1991@mail.ru>
2023-01-27 21:02:51 +0300

    add random

--- Exampele003_Sum/Program.cs
+++ Exampele003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 31;
-int nunberB = 5;
+﻿int numberA = new Random().Next(1, 10); //1 2 3 ...9
+Console.WriteLine(numberA);
+int nunberB = new Random().Next(1, 10);
+Console.WriteLine(nunberB);
 int result = numberA + nunberB;
 Console.WriteLine(result);
